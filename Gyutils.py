29be0ef4cc3969f36268ba9409d95f2875fe86fa
LHY-hayes <775788531@qq.com>
2019-08-25 11:19:02 +0800

    init commit

--- Gyutils.py
+++ Gyutils.py
@@ -11,7 +11,8 @@ import requests
 import random
 from bs4 import BeautifulSoup
 
-def logger(self):
+# 记录日志
+def logger():
     logging.basicConfig(level=logging.INFO,  # 控制台打印的日志级别
                         # when='D',  #when是间隔的时间单位，单位有以下几种：S 秒 M 分 H 小时、 D 天、 W 每星期（interval==0时代表星期一） midnight 每天凌晨
                         filename='movies.log',
@@ -24,9 +25,25 @@ def logger(self):
     console.setFormatter(formatter)
     logging.getLogger("").addHandler(console)
 
+def request_get(url):
 
-def request_url(self, url):
-    self.User_Agents = [
+    r = requests.get(url, headers=header, timeout=10)
+    if r.status_code is 200:
+        # logging.info("数据加载完成........")
+        soup = BeautifulSoup(r.content, 'lxml')
+        return soup
+    else:
+        return None
+def request_post(url, data):
+    r = requests.get(url, headers=header, data=data , timeout=10)
+    if r.status_code is 200:
+        # logging.info("数据加载完成........")
+        soup = BeautifulSoup(r.content, 'lxml')
+        return soup
+    else:
+        return None
+
+User_Agents = [
         "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
         "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
         "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -43,26 +60,15 @@ def request_url(self, url):
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11",
         "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.20 (KHTML, like Gecko) Chrome/19.0.1036.7 Safari/535.20",
         "Opera/9.80 (Macintosh; Intel Mac OS X 10.6.8; U; fr) Presto/2.9.168 Version/11.52"]
-    self.header = {
+header = {
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'zh-CN,zh;q=0.8',
         'Cache-Control': 'no-cache',
         'Connection': 'keep-alive',
         'Pragma': 'no-cache',
-        'Referer': self.referer,
-        'User-Agent': random.choice(self.User_Agents)
+        # 'Referer': referer,
+        'User-Agent': random.choice(User_Agents)
     }
-    r = requests.get(url, headers=self.header, timeout=10)
-    if r.status_code is 200:
-        # logging.info("数据加载完成........")
-        soup = BeautifulSoup(r.content, 'lxml')
-        return soup
-    else:
-        return None
-
-
-
-
 
 
---
+++ MainTask.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*- ＃
+#==========================
+# Name          MainTask.py
+# Description:  采集主类
+# Author:       hayes
+# Date:         2019/8/25 11:15
+#==========================
+
+import Gyutils as utils
+class MainTask():
+
+
+    utils.request_get("")
+
+
+
+
+
+if __name__ == '__main__':
+
+
+   mt =  MainTask()
+
+
+
