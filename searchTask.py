b48c4d301283f6b82fc961174613fac7160da6fe
lzx <lzx007.love@163.com>
2019-08-26 20:10:36 +0800

    init commit

--- searchTask.py
+++ searchTask.py
@@ -6,10 +6,11 @@
 
 import GyUtils
 import sys
+import MainTask
+import logging
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-
 
 class doTask():
     def __init__(self, data):
@@ -33,13 +34,40 @@ class doTask():
         return data
 
 
+
+
 if __name__ == '__main__':
 
-    # your_input = str(raw_input('please input your want:'))
+    href_film = ''
 
-    your_input = "使徒行者"
+    your_input = str(raw_input('please input your want:'))
+    if your_input:
+        do = doTask(your_input);
+        data = do.action();
+        for i in range(len(data)):
+            print "标号:", i+1 , "   >>> 资源 : ", data[i][0], data[i][1] ,data[i][2]
 
-    do = doTask(your_input);
-    data = do.action();
-    for name, column, href in data:
-        print  name,"\t", column,"\t", href
+        row = input('请输入前面的标号:(0 不做选择)')
+        if row:
+            href_film = data[row-1][2]
+
+
+
+    if href_film > -1:
+        m = MainTask.MainTask()
+        filmInfo = m.action_step_two(href_film)
+        bof_urls = filmInfo["film_url"].split("#")
+        filmInfo.pop("film_url")
+        sql = GyUtils.dict_2_insert_sql(filmInfo, "t_movies")
+        logging.info(sql)
+        cursor = GyUtils.mysql_connect_cursor()
+        cursor.execute(sql)
+
+        for bof_url in bof_urls:
+            sql_url = "insert into t_movies_url (uuid,film_name,film_url)values ('%s' , '%s' , '%s' )" \
+                      % (filmInfo["uuid"], filmInfo["film_name"], bof_url)
+            logging.info(sql_url)
+            cursor.execute(sql_url)
+        cursor.execute("commit")
+        logging.info("<<{}>>  save done".format(filmInfo["film_name"]))
+
